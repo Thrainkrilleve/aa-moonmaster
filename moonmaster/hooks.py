948c6d94fa6e7d9cf16772b046f9aa268d99fdf7
Your Name <your@email.com>
2026-04-02 21:00:26 -0600

    fix: exclude all views from main_character_required; show menu to superusers

--- moonmaster/hooks.py
+++ moonmaster/hooks.py
@@ -16,7 +16,7 @@ class MoonMasterMenuItem(MenuItemHook):
         )
 
     def render(self, request):
-        if request.user.has_perm("moonmaster.basic_access"):
+        if request.user.is_superuser or request.user.has_perm("moonmaster.basic_access"):
             return super().render(request)
         return ""
 
@@ -28,4 +28,21 @@ def register_menu():
 
 @hooks.register("url_hook")
 def register_urls():
-    return UrlHook(urls, "moonmaster", r"^moonmaster/")
+    return UrlHook(
+        urls,
+        "moonmaster",
+        r"^moonmaster/",
+        excluded_views=[
+            "moonmaster.views.dashboard",
+            "moonmaster.views.moon_list",
+            "moonmaster.views.moon_detail",
+            "moonmaster.views.extractions",
+            "moonmaster.views.metenox_list",
+            "moonmaster.views.reports",
+            "moonmaster.views.manage_owners",
+            "moonmaster.views.add_owner",
+            "moonmaster.views.remove_owner",
+            "moonmaster.views.moon_profitability_api",
+            "moonmaster.views.refresh_prices_api",
+        ],
+    )
